ca715419d727016e8cf4d888a9860552373c36dc
sixzjkk <jenifergmporfirio@gmail.com>
2025-09-11 17:22:59 -0400

    Config tailwindcss

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RegisterUser from './pages/RegisterUser';
 import LoginUser from './pages/LoginUser'
 import App from './App';
+import './styles/main.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
