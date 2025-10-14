6790f337922639d68bf01bcb31ef078347727fe4
sixzjkk <jenifer.porfirio@estudante.ifms.edu.br>
2025-10-14 16:29:57 -0400

    Update userController.js to UserController.js

---
+++ BackEnd/controllers/UserController.js
@@ -0,0 +1,86 @@
+import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { PrismaClient } from '@prisma/client';
+
+const client = new PrismaClient();
+
+class UserController {
+    static async loginUser (req, res) {
+        const { email, password } = req.body;
+
+        const user = await client.user.findUnique({
+            where: {
+                email
+            }
+        });
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'User not found!',
+                error: true
+            })
+        }
+
+        const correctPassword = bcryptjs.compareSync(password, user.password);
+
+        if (!correctPassword) {
+            return res.status(401).json({
+                message: 'Incorrect password!',
+                error: true
+            });
+        }
+
+        const token = jwt.sign({id: user.id}, process.env.SECRET_KEY, {expiresIn: '2h'});
+
+        return res.status(200).json({
+            message: 'Authenticated!',
+            error: false,
+            token
+        });
+    }
+
+    static async registerUser (req, res) {
+        const { name, email, password, confirmPassword } = req.body;
+
+        const possibleUser = await client.user.findUnique({
+            where: {
+                email
+            }
+        });
+
+        if (possibleUser) {
+            return res.status(409).json({
+                message: 'Email already registered!',
+                error: true
+            })
+        }
+
+        const salt = bcryptjs.genSaltSync(8);
+        const hashPassword = bcryptjs.hashSync(password, salt);
+
+        if (!name || !email || !password || !confirmPassword || (confirmPassword !== password)) {
+            return res.status(400).json({
+                message: 'All fields are required!',
+                error: true
+            });
+        }
+
+        const user = await client.user.create({
+            data: {
+                name,
+                email,
+                password: hashPassword
+            }
+        });
+
+        const token = jwt.sign({id: user.id}, process.env.SECRET_KEY, {expiresIn: '2h'});
+
+        return res.status(200).json({
+            message: 'Registration successful!',
+            error: false,
+            token 
+        });
+    }
+}
+
+export { UserController };
