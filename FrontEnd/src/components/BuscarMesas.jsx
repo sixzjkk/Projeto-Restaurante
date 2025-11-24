67a5336cca419f9ce14c5d95acc3aaf701fa113c
sixzjkk <jenifer.porfirio@estudante.ifms.edu.br>
2025-11-23 21:21:10 -0400

    Update cardapio CSS

--- FrontEnd/src/components/BuscarMesas.jsx
+++ FrontEnd/src/components/BuscarMesas.jsx
@@ -1,3 +1,5 @@
+import styles from '../styles/buscarMesas.module.css';
+import imgStar from '../assets/image-star.png';
 import { useState, useEffect } from 'react';
 
 export default function BuscarMesas() {
@@ -7,35 +9,44 @@ export default function BuscarMesas() {
         const buscar = async () => {
             try {
                 const res = await fetch(`${import.meta.env.VITE_API_URL}/mesas`);
-
                 const data = await res.json();
-
                 setMesas(data.mesas);
             } catch (err) {
                 console.log(err);
             }
-        }
+        };
 
         buscar();
-    })
+    }, []);
 
     if (!mesas) {
         return <p>Carregando...</p>;
     }
 
     return (
-    <>
-        <table>
-            {
-            mesas.map(mesa => (
-                <tr key={mesa.id}>
-                    <td>MESA {mesa.id}</td>
-                    <td>CÓDIGO {mesa.codigo}</td>
-                    <td>{mesa.n_lugares} LUGARES</td>
-                </tr>
-            ))
-        }
-        </table>
-    </>
+        <>
+            <div id='buscarMesas' className={styles.containerPrincipalQuarto}>
+                <h1 className={styles.titleBuscarMesas}>Consultar Mesas</h1>
+                <div className={styles.containerBuscarMesas}>
+                    <div className={styles.containerTextBuscarMesas}>
+                        <h2 className={styles.subtitleBuscarMesas}>Consultar Mesas</h2>
+                        <img className={styles.imageStar} src={imgStar} />
+
+                        <table>
+                            <tbody>
+                                {mesas.map(mesa => (
+                                    <tr className={styles.listaMesas} key={mesa.id}>
+                                        <td className={styles.mesas}>MESA {mesa.id}</td>
+                                        <td className={styles.mesas}>CÓDIGO {mesa.codigo}</td>
+                                        <td className={styles.mesas}>{mesa.n_lugares} LUGARES</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
